128bb8f31c19ddae9a9eaed039b36fdff4d413f8
mikemcm151 <mikemcm151@gmail.comgit config --global user.name mikemcm151git config --global user.email mikemcm151@gmail.com>
2017-12-19 22:34:16 -0600

    get html working

--- server.js
+++ server.js
@@ -1,15 +1,43 @@
 var express = require("express");
+var bodyParser = require('body-parser');
 var path = require("path");
 var PORT = process.env.PORT || 5000;
 var app = express();
 
 app.get("/", function(req, res){
-	app.use(express.static(__dirname + '/styles'));
-	app.listen(5000)
-	// res.sendFile(path.join(__dirname, "home.html"))
+	// app.use(express.static(__dirname, + './app/public'));
+	// app.listen(5000)
+	res.sendFile(path.join(__dirname, "/app/public/home.html"));
 });
+
+//ROUTER
+// require('./app/routing/apiRoutes.js')(app); 
+// require('./app/routing/htmlRoutes.js')(app);
 
 app.listen(PORT)
 
   // Log (server-side) when our server has started
   console.log("Server listening on: http://localhost:" + PORT);
+
+//Dependencies:
+// var express = require('express');
+// var bodyParser = require('body-parser');
+// var path = require('path');
+
+// var app = express();
+// var PORT = process.env.PORT || 5000;
+
+// // Sets up the Express app to handle data parsing
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.text());
+// app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+
+// //ROUTER
+// require('./app/routing/apiRoutes.js')(app); 
+// require('./app/routing/htmlRoutes.js')(app);
+
+// // Starts the server to begin listening
+// app.listen(PORT, function () {
+//   console.log('App listening on PORT: ' + PORT);
+// });
