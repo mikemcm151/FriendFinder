baf60eac41306316d0dbe14c98480871ab499db9
mikemcm151 <mikemcm151@gmail.comgit config --global user.name mikemcm151git config --global user.email mikemcm151@gmail.com>
2017-12-19 18:30:27 -0600

    Revisions

--- server.js
+++ server.js
@@ -4,7 +4,12 @@ var PORT = process.env.PORT || 5000;
 var app = express();
 
 app.get("/", function(req, res){
-	res.sendFile(path.join(__dirname, "home.html"))
+	app.use(express.static(__dirname + '/styles'));
+	app.listen(5000)
+	// res.sendFile(path.join(__dirname, "home.html"))
 });
 
 app.listen(PORT)
+
+  // Log (server-side) when our server has started
+  console.log("Server listening on: http://localhost:" + PORT);
