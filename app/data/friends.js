9ab31ce92e2cc680553b858ecb447626fa00e789
mikemcm151 <mikemcm151@gmail.comgit config --global user.name mikemcm151git config --global user.email mikemcm151@gmail.com>
2017-12-19 19:29:24 -0600

    revised

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,5 +1,6 @@
+ var friendsArray = [
 {
-  "name":"Ahmed",
+  "name":"Billy",
   "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
   "scores":[
       5,
@@ -64,4 +65,5 @@
       4,
       1
     ]
-}
+  }
+];
